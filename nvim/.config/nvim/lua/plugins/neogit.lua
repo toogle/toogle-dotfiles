a5965a706b4c3a2497fa45cf439c9d42c128712a
toogle <tooogle@mail.ru>
2025-05-18 20:41:27 +0300

    [nvim] Add keymaps for Neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,10 +1,17 @@
 return {
   'NeogitOrg/neogit',
-  event = 'VeryLazy',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'sindrets/diffview.nvim',
     'nvim-telescope/telescope.nvim'
   },
-  opts = {}
+  opts = {},
+  cmd = 'Neogit',
+  keys = {
+    { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
+    { '<leader>gc', '<cmd>Neogit commit<cr>', desc = 'Neogit commit' },
+    { '<leader>gd', '<cmd>Neogit diff<cr>', desc = 'Neogit diff' },
+    { '<leader>gl', '<cmd>Neogit log<cr>', desc = 'Neogit log' },
+    { '<leader>gs', '<cmd>Neogit stash<cr>', desc = 'Neogit stash' }
+  }
 }
