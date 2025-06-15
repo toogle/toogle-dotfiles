e5d9279ab6af826c34017508077fc92303aba802
toogle <tooogle@mail.ru>
2025-06-15 19:29:10 +0300

    [nvim] Add MeanderingProgrammer/render-markdown.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons'
+  },
+  opts = {},
+  ft = 'markdown'
+}
